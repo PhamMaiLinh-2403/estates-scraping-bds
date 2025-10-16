d99cbffff8871f65398e7bd8305dcca67ebc84d0
OS TDTS <OS TDTS@HO-TDTS-03.mbamc.local>
2025-10-16 17:20:27 +0700

    add creating worker to selenium manager

--- src/selenium_manager.py
+++ src/selenium_manager.py
@@ -1,5 +1,10 @@
+import time 
+import random
+import threading
 from seleniumbase import Driver
+
 from . import config
+from .scraping import Scraper
 
 
 def create_stealth_driver(headless: bool = True) -> Driver:
@@ -16,3 +21,37 @@ def create_stealth_driver(headless: bool = True) -> Driver:
     driver.set_window_size(width, height)
 
     return driver
+
+def scrape_worker(worker_id: int, url_subset: list[str], existing_ids: set[str], stop_event: threading.Event) -> list[dict]:
+    """
+    Defines the task for a single scraping worker.
+    Each worker gets its own driver & scraper instance.
+    """
+    base = config.SCRAPING_DETAILS_CONFIG.get("stagger_step_sec", 2.0)
+    start_delay = worker_id * base
+    print(f"[Worker {worker_id}]: Sleeping {start_delay:.1f}s before start.")
+    time.sleep(start_delay)
+
+    driver = create_stealth_driver(headless=config.SELENIUM_CONFIG["headless"])
+    scraper = Scraper(driver)
+    results = []
+
+    for idx, url in enumerate(url_subset, 1):
+        print(f"[Worker {worker_id}]  {idx}/{len(url_subset)}  → {url}")
+        data = scraper.scrape_listing_details(url)
+
+        if data:
+            listing_id = str(data.get("id")).replace(".0", "") 
+            if listing_id in existing_ids:
+                print(f"[Worker {worker_id}] Skipping already-scraped ID: {listing_id}")
+                continue
+            results.append(data)
+
+        if config.SCRAPING_DETAILS_CONFIG["stagger_mode"] == "random":
+            delay = random.uniform(
+                config.SCRAPING_DETAILS_CONFIG["stagger_step_sec"],
+                config.SCRAPING_DETAILS_CONFIG["stagger_max_sec"],
+            )
+            time.sleep(delay)
+    driver.quit()
+    return results
