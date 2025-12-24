9fe1b3bb737d9796f48249e4e47c61aa773b3f57
OS TDTS <OS TDTS@HO-TDTS-04.vna.local>
2025-12-24 17:13:12 +0700

    add ui code

--- ui.py
+++ ui.py
@@ -1,4 +1,33 @@
-from fastapi import FastAPI
-from pydantic import Query, Path
+from fastapi import FastAPI, Request, Query, Path, Form
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 from typing import Annotated
+from fastapi.templating import Jinja2Templates
 
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse(
+        "home.html", 
+        {"request": request}
+    )
+
+@app.post("/submit", response_class=HTMLResponse)
+async def submit(
+    request: Request,
+    web: str = Form(...),
+    start_time: str = Form(...),
+    end_time: str = Form(...)
+):
+    return templates.TemplateResponse(
+        "results.html",
+        {
+            "request": request,
+            "web": web,
+            "start_time": start_time,
+            "end_time": end_time
+        }
+    )
